1a927767e91f26b5ef483c76b3e06bbf6ba7d8e4
lschirripa <72322637+lschirripa@users.noreply.github.com>
2022-10-21 21:28:42 -0300

    fix constraint errors

--- Creacion_de_Tablas.sql
+++ Creacion_de_Tablas.sql
@@ -50,7 +50,7 @@ CREATE TABLE [GD2C2022].[sale_worson].Venta
 	VENTA_TOTAL decimal(18,2),
 	VENTA_MEDIO_ENVIO_ID decimal,	--FK a Medio_envio.VENTA_MEDIO_ENVIO_ID
 	CANAL_VENTA_ID decimal,			--FK a Canal_venta.CANAL_VENTA_TIPO
-	CLIENTE_ID decimal(18,2)		--FK CLiente
+	CLIENTE_ID decimal(18,2),		--FK CLiente
 	CONSTRAINT ck_venta_total CHECK(VENTA_TOTAL > 0)	--CONTRAIN DE TOTAL MAYOR A CERO 
 )
 
@@ -108,7 +108,7 @@ CREATE TABLE [GD2C2022].[sale_worson].Venta_por_descuento
 
 CREATE TABLE [GD2C2022].[sale_worson].Descuento_venta
 (
-	VENTA_DESCUENTO_ID decimal(18,2) PRIMARY KEY IDENTITY(1,1), --lo creamos nosotros
+	VENTA_DESCUENTO_ID int PRIMARY KEY IDENTITY(1,1), --lo creamos nosotros
 	VENTA_DESCUENTO_IMPORTE decimal(18,2) NOT NULL,
 	VENTA_DESCUENTO_CONCEPTO nvarchar(510)
 )
@@ -148,7 +148,7 @@ CREATE TABLE [GD2C2022].[sale_worson].Compra
 	COMPRA_TOTAL decimal(18,2),
 	COMPRA_PROVEEDOR_ID decimal,	--FK a Proveedor.CODIGO_PROVEEDOR
 	COMPRA_FECHA date,
-	COMPRA_MEDIO_PAGO nvarchar(510)
+	COMPRA_MEDIO_PAGO nvarchar(510),
 	CONSTRAINT ck_compra_total CHECK(COMPRA_TOTAL > 0)		 --CONTRAIN DE TOTAL MAYOR A CERO 
 )
 
@@ -181,7 +181,7 @@ CREATE TABLE [GD2C2022].[sale_worson].Proveedor
 
 CREATE TABLE [GD2C2022].[sale_worson].Descuento_compra
 (
-	COMPRA_DESCUENTO_ID decimal PRIMARY KEY IDENTITY(1,1), --lo creamos nosotros,
+	COMPRA_DESCUENTO_ID int PRIMARY KEY IDENTITY(1,1), --lo creamos nosotros,
 	DESCUENTO_COMPRA_CODIGO decimal(19,0),
 	DESCUENTO_COMPRA_VALOR decimal(18,2)
 )
