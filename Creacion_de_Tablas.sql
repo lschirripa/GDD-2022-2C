e97108eee49876ed6ce596ff32e74cc19b2bc3f7
Francisco Maver <fmaver@frba.utn.edu.ar>
2022-10-27 16:12:44 -0300

    Update Creacion_de_Tablas.sql

--- Creacion_de_Tablas.sql
+++ Creacion_de_Tablas.sql
@@ -29,7 +29,7 @@ CREATE TABLE [GD2C2022].[sale_worson].producto
 CREATE TABLE [GD2C2022].[sale_worson].producto_variante
 ( 
 	PV_CODIGO					nvarchar(100) PRIMARY KEY,
-	PV_VARIANTE					decimal(18,0),			-- FK a variante.VARIANTE_ID		--lo creamos nosotros
+	PV_VARIANTE					decimal(18,0),		-- FK a variante.VARIANTE_ID		--lo creamos nosotros
 	PV_PRODUCTO					nvarchar(100),		-- FK a producto.PRODUCTO_CODIGO	--lo creamos nosotros
 	PV_PRECIO_UNITARIO_ACTUAL	decimal(18,2),											--lo creamos nosotros
 	PV_STOCK_ACTUAL				decimal(18,0)											--lo creamos nosotros
@@ -39,7 +39,7 @@ CREATE TABLE [GD2C2022].[sale_worson].producto_variante
 
 CREATE TABLE [GD2C2022].[sale_worson].venta
 (
-	VENTA_CODIGO			decimal(18,0) NOT NULL PRIMARY KEY,
+	VENTA_CODIGO			decimal(18,0) PRIMARY KEY,
 	VENTA_FECHA				date,
 	VENTA_MEDIO_PAGO		decimal,					--FK a medio_pago.MEDIO_PAGO_ID		--lo creamos nosotros
 	VENTA_MEDIO_ENVIO		decimal,					--FK a medio_envio.MEDIO_ENVIO_ID	--lo creamos nosotros
@@ -56,7 +56,7 @@ CREATE TABLE [GD2C2022].[sale_worson].venta_detalle
 	VD_PV				nvarchar(100),			--FK a producto_variante.PV_CODIGO		--lo creamos nosotros
 	VD_PV_CANTIDAD		decimal(18,0),
 	VD_PV_PRECIO		decimal(18,2),													--lo creamos nosotros
-	PRIMARY KEY (VENTA_CODIGO, PRODUCTO_VARIANTE_CODIGO)
+	PRIMARY KEY (VD_VENTA, VD_PV)
 )
 
 -------------------------------------------------------------------------------------------
@@ -82,10 +82,10 @@ CREATE TABLE [GD2C2022].[sale_worson].canal_venta
 
 CREATE TABLE [GD2C2022].[sale_worson].venta_x_cupon
 (
-	VXC_VENTA				decimal(18,O),	--FK a venta.VENTA_CODIGO				--lo creamos nosotros
-	VXC_CUPON				nvarchar(510),	--FK a cupon_venta.CUPON_VENTA_CODIGO	--lo creamos nosotros
+	VXC_VENTA				decimal(18,0),	--FK a venta.VENTA_CODIGO				--lo creamos nosotros
+	VXC_CUPON				nvarchar(255),	--FK a cupon_venta.CUPON_VENTA_CODIGO	--lo creamos nosotros
 	VXC_IMPORTE				decimal(18,2), 
-	PRIMARY KEY (VENTA_CODIGO, VENTA_CUPON_CODIGO)
+	PRIMARY KEY (VXC_VENTA, VXC_CUPON)
 )
 
 -------------------------------------------------------------------------------------------
@@ -114,7 +114,7 @@ CREATE TABLE [GD2C2022].[sale_worson].venta_x_descuento
 CREATE TABLE [GD2C2022].[sale_worson].descuento_venta
 (
 	DESCUENTO_VENTA_ID			decimal PRIMARY KEY IDENTITY(1,1),	--lo creamos nosotros
-	DESCUENTO_VENTA_IMPORTE		decimal(18,2) NOT NULL,
+	DESCUENTO_VENTA_IMPORTE		decimal(18,2),
 	DESCUENTO_VENTA_CONCEPTO	nvarchar(510)
 )
 
@@ -153,7 +153,7 @@ CREATE TABLE [GD2C2022].[sale_worson].compra
 	COMPRA_FECHA		date,
 	COMPRA_MEDIO_PAGO	nvarchar(510),
 	COMPRA_TOTAL		decimal(18,2),
-	--CONSTRAINT ck_compra_total CHECK(COMPRA_TOTAL > 0)	--CONSTRAINT DE TOTAL MAYOR A CERO 
+	--CONSTRAINT ck_compra_total CHECK(COMPRA_TOTAL > 0)	--CONSTRAINT DE TOTAL MAYOR A CERO --> Puede haber compras en las que el total sea nulo por x motivo
 )
 
 -------------------------------------------------------------------------------------------
@@ -173,7 +173,7 @@ CREATE TABLE [GD2C2022].[sale_worson].compra_x_descuento
 (
 	CXD_COMPRA		decimal(18,0),		--FK a compra.COMPRA_NUMERO							--lo creamos nosotros
 	CXD_DESCUENTO	decimal(18,0),		--FK a descuento_compra.DESCUENTO_COMPRA_CODIGO		--lo creamos nosotros
-	PRIMARY KEY (COMPRA_NUMERO, COMPRA_DESCUENTO_ID)
+	PRIMARY KEY (CXD_COMPRA, CXD_DESCUENTO)
 )
 
 -------------------------------------------------------------------------------------------
