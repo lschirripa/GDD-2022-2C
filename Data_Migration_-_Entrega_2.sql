e1b90d0edd34d90faff0a459ef97db68f2ed8bb9
Francisco Maver <fmaver@frba.utn.edu.ar>
2022-10-28 17:39:06 -0300

    Update Data_Migration_-_Entrega_2.sql

--- Data_Migration_-_Entrega_2.sql
+++ Data_Migration_-_Entrega_2.sql
@@ -1,6 +1,8 @@
-------------------------------------------------------------------------------------------------------------------------
------------------------------------------------------- MIGRACION -------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------ MIGRACION -------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------
+-- RECORDAR QUE CUANDO SE CORRA LA MIGRACION, ES IMPORTANTE MANTENER ESTE ORDEN DADO QUE LA MAYORIA DE LAS TABLAS TIENEN FK --
+------------------------------------------------------------------------------------------------------------------------------
 /*
 BEGIN TRANSACTION
 	--INSERT INTO)
@@ -13,19 +15,21 @@ BEGIN TRANSACTION
 ROLLBACK*/
 
 -- Reset AutoIncrement
-DBCC CHECKIDENT ('sale_worson.Variante', RESEED, 0)
-DBCC CHECKIDENT ('sale_worson.Variante', RESEED, 0)
+--DBCC CHECKIDENT ('sale_worson.Variante', RESEED, 0)
+--DBCC CHECKIDENT ('sale_worson.Variante', RESEED, 0)
 
 
 -------------------------------------------------------- VARIANTE ------------------------
 
 BEGIN TRANSACTION
-	INSERT INTO sale_worson.Variante
+	INSERT INTO [sale_worson].[Variante]
+
 	SELECT DISTINCT 
 		PRODUCTO_TIPO_VARIANTE,
 		PRODUCTO_VARIANTE
 	FROM [gd_esquema].[Maestra]
 	WHERE PRODUCTO_VARIANTE_CODIGO IS NOT NULL
+
 --COMMIT
 ROLLBACK
 
@@ -58,7 +62,7 @@ SELECT * FROM [sale_worson].[producto]
 
 -------------------------------------------------------- PRODUCTO VARIANTE -----------------------
 BEGIN TRANSACTION
-INSERT INTO [sale_worson].[producto_variante]
+	INSERT INTO [sale_worson].[producto_variante]
 
 	SELECT DISTINCT
 		maa.PRODUCTO_VARIANTE_CODIGO as PV_CODIGO,
@@ -145,7 +149,8 @@ BEGIN TRANSACTION
 			VENTA_CANAL_COSTO,
 			VENTA_CANAL
 	FROM [gd_esquema].[Maestra]
-	WHERE VENTA_CANAL is not null
+	WHERE VENTA_CANAL IS NOT NULL
+
 --COMMIT
 ROLLBACK
 
@@ -246,31 +251,121 @@ ROLLBACK
 SELECT * FROM [sale_worson].[venta]
 -----------------------------------
 
+
 -------------------------------------------------------- VENTA DETALLE -------------------------
+BEGIN TRANSACTION
+	INSERT INTO [sale_worson].[venta_detalle]
 
--------------------------------------------------------- COMPRA DETALLE-------------------------
+	SELECT DISTINCT
+		v.VENTA_CODIGO VD_VENTA,
+		p.PV_CODIGO VD_PV,
+		SUM(m.VENTA_PRODUCTO_CANTIDAD) VD_PV_CANTIDAD,
+		SUM(m.VENTA_PRODUCTO_PRECIO * m.VENTA_PRODUCTO_CANTIDAD) as VD_PV_PRECIO
 
--------------------------------------------------------- COMPRA  -------------------------------
+	FROM [gd_esquema].[Maestra] m
 
+	INNER JOIN [sale_worson].[producto_variante] p ON m.PRODUCTO_VARIANTE_CODIGO = p.PV_CODIGO
+	INNER JOIN [sale_worson].[venta] v ON m.VENTA_CODIGO = v.VENTA_CODIGO
+
+	GROUP BY v.VENTA_CODIGO, PV_CODIGO
+
+--COMMIT
+ROLLBACK
+
+-------------------------------------------
+SELECT * FROM [sale_worson].[venta_detalle]
+-------------------------------------------
+
+
+-------------------------------------------------------- CUPON VENTA----------------------------
+BEGIN TRANSACTION
+	
+	INSERT INTO [sale_worson].[cupon_venta]
+   
+	SELECT DISTINCT
+		VENTA_CUPON_CODIGO,
+		VENTA_CUPON_FECHA_DESDE,
+		VENTA_CUPON_FECHA_HASTA,
+		VENTA_CUPON_VALOR,
+		VENTA_CUPON_TIPO 
+		
+	FROM [gd_esquema].[Maestra]
+	WHERE VENTA_CUPON_CODIGO IS NOT NULL
+
+--COMMIT
+ROLLBACK
+
+-----------------------------------------
+SELECT * FROM [sale_worson].[cupon_venta]
+-----------------------------------------
 
 
 -------------------------------------------------------- VENTA X CUPON -------------------------
+BEGIN TRANSACTION
+	INSERT INTO [sale_worson].[venta_x_cupon]
+
+	SELECT DISTINCT
+		v.VENTA_CODIGO,
+		cv.CUPON_VENTA_CODIGO,
+		m.VENTA_CUPON_IMPORTE
+	FROM [gd_esquema].[Maestra] m
+
+	INNER JOIN [sale_worson].[venta] v ON m.VENTA_CODIGO = v.VENTA_CODIGO
+	INNER JOIN [sale_worson].cupon_venta cv ON m.VENTA_CUPON_CODIGO = cv.CUPON_VENTA_CODIGO
+
+--COMMIT
+ROLLBACK
+
+-------------------------------------------
+SELECT * FROM [sale_worson].[venta_x_cupon]
+-------------------------------------------
+
+-------------------------------------------------------- DESCUENTO VENTA -----------------------
+BEGIN TRANSACTION
+	INSERT INTO [sale_worson].[descuento_venta]
+   
+	SELECT DISTINCT 
+			VENTA_DESCUENTO_IMPORTE,
+			VENTA_DESCUENTO_CONCEPTO
+		
+	FROM [gd_esquema].[Maestra]
+	WHERE VENTA_DESCUENTO_IMPORTE IS NOT NULL
+
+--COMMIT
+ROLLBACK
+
+---------------------------------------------
+SELECT * FROM [sale_worson].[descuento_venta]
+---------------------------------------------
+
+
 -------------------------------------------------------- VENTA X DESCUENTO ---------------------
+BEGIN TRANSACTION
+	
+	INSERT INTO [sale_worson].[venta_x_descuento]
+
+	SELECT DISTINCT
+		v.VENTA_CODIGO,
+		dv.DESCUENTO_VENTA_ID
+	FROM [gd_esquema].[Maestra] m
+
+	INNER JOIN [sale_worson].[venta] v ON v.VENTA_CODIGO = m.VENTA_CODIGO
+	INNER JOIN [sale_worson].[descuento_venta] dv ON dv.DESCUENTO_VENTA_IMPORTE = m.VENTA_DESCUENTO_IMPORTE AND dv.DESCUENTO_VENTA_CONCEPTO = m.VENTA_DESCUENTO_CONCEPTO
 
 
--------------------------------------------------------- COMPRA X DESCUENTO --------------------
+--COMMIT
+ROLLBACK
+
+-----------------------------------------------
+SELECT * FROM [sale_worson].[venta_x_descuento]
+-----------------------------------------------
+
+
 
 -------------------------------------------------------- PROVEEDOR  ----------------------------
 BEGIN TRANSACTION
 	
-INSERT INTO [sale_worson].[proveedor]
-           ([PROVEEDOR_RAZON_SOCIAL]
-           ,[PROVEEDOR_CUIT]
-           ,[PROVEEDOR_DOMICILIO]
-           ,[PROVEEDOR_MAIL]
-           ,[PROVEEDOR_LOCALIDAD]
-           ,[PROVEEDOR_CODIGO_POSTAL]
-           ,[PROVEEDOR_PROVINCIA])
+	INSERT INTO [sale_worson].[proveedor]
    
 	SELECT DISTINCT 
 			PROVEEDOR_RAZON_SOCIAL
@@ -282,59 +377,112 @@ INSERT INTO [sale_worson].[proveedor]
            ,PROVEEDOR_PROVINCIA
 		
 	FROM [gd_esquema].[Maestra]
-	WHERE PROVEEDOR_RAZON_SOCIAL is not null
+	WHERE PROVEEDOR_RAZON_SOCIAL IS NOT NULL
+
 --COMMIT
 ROLLBACK
--------------------------------------------------------- CUPON VENTA----------------------------
+
+---------------------------------------
+SELECT * FROM [sale_worson].[proveedor]
+---------------------------------------
+
+
+
+-------------------------------------------------------- COMPRA  -------------------------------
+
 BEGIN TRANSACTION
-	
-INSERT INTO [sale_worson].[cupon_venta]
-           ([CUPON_VENTA_CODIGO]
-           ,[CUPON_VENTA_FECHA_INICIO]
-           ,[CUPON_VENTA_FECHA_FIN]
-           ,[CUPON_VENTA_VALOR]
-           ,[CUPON_VENTA_TIPO])
-   
+	INSERT INTO [sale_worson].[compra]
+
 	SELECT DISTINCT
-		VENTA_CUPON_CODIGO,
-		VENTA_CUPON_FECHA_DESDE,
-		VENTA_CUPON_FECHA_HASTA,
-		VENTA_CUPON_VALOR,
-		VENTA_CUPON_TIPO 
-		
-	FROM [gd_esquema].[Maestra]
-	WHERE VENTA_CUPON_CODIGO is not null
+		mt.COMPRA_NUMERO,
+		p.PROVEEDOR_ID,
+		mt.COMPRA_FECHA,
+		mt.COMPRA_MEDIO_PAGO,
+		mt.COMPRA_TOTAL
+
+	FROM [gd_esquema].[Maestra] mt
+
+	INNER JOIN [sale_worson].[proveedor] p
+	ON	mt.PROVEEDOR_RAZON_SOCIAL	= p.PROVEEDOR_RAZON_SOCIAL		AND
+		mt.PROVEEDOR_CUIT			= p.PROVEEDOR_CUIT				AND
+		mt.PROVEEDOR_DOMICILIO		= p.PROVEEDOR_DOMICILIO			AND
+		mt.PROVEEDOR_MAIL			= p.PROVEEDOR_MAIL				AND
+		mt.PROVEEDOR_LOCALIDAD		= p.PROVEEDOR_LOCALIDAD			AND
+		mt.PROVEEDOR_CODIGO_POSTAL	= p.PROVEEDOR_CODIGO_POSTAL		AND
+		mt.PROVEEDOR_PROVINCIA		= p.PROVEEDOR_PROVINCIA
+
+	WHERE mt.COMPRA_NUMERO IS NOT NULL
+
 --COMMIT
 ROLLBACK
 
+------------------------------------
+SELECT * FROM [sale_worson].[compra]
+------------------------------------
 
--------------------------------------------------------- DESCUENTO VENTA -----------------------
+
+-------------------------------------------------------- COMPRA DETALLE-------------------------
 BEGIN TRANSACTION
-	INSERT INTO [sale_worson].[descuento_venta]
-           ([DESCUENTO_VENTA_IMPORTE]
-           ,[DESCUENTO_VENTA_CONCEPTO])
-   
-	SELECT DISTINCT 
-			VENTA_DESCUENTO_IMPORTE,
-			VENTA_DESCUENTO_CONCEPTO
-		
-	FROM [gd_esquema].[Maestra]
-	WHERE VENTA_DESCUENTO_IMPORTE is not null
+	INSERT INTO [sale_worson].[compra_detalle]
+
+	SELECT DISTINCT
+		c.COMPRA_NUMERO as CD_COMPRA,
+		p.PV_CODIGO as CD_PV,
+		SUM(m.COMPRA_PRODUCTO_CANTIDAD) as CD_CANTIDAD,
+		SUM(m.COMPRA_PRODUCTO_PRECIO * m.COMPRA_PRODUCTO_CANTIDAD) as CD_PRECIO
+
+	FROM [gd_esquema].[Maestra] m
+
+	INNER JOIN [sale_worson].[producto_variante] p ON m.PRODUCTO_VARIANTE_CODIGO = p.PV_CODIGO
+	INNER JOIN [sale_worson].[compra] c ON m.COMPRA_NUMERO = c.COMPRA_NUMERO 
+
+	GROUP BY c.COMPRA_NUMERO, p.PV_CODIGO
+
+
 --COMMIT
 ROLLBACK
+
+--------------------------------------------
+SELECT * FROM [sale_worson].[compra_detalle]
+--------------------------------------------
+
 
 -------------------------------------------------------- DESCUENTO COMPRA-----------------------
 
--- son todos null en la maestra 
 BEGIN TRANSACTION
 	INSERT INTO [sale_worson].[descuento_compra]
-           ([DESCUENTO_COMPRA_CODIGO]
-           ,[DESCUENTO_COMPRA_PORCENTAJE])
    
 	SELECT DISTINCT 
 			DESCUENTO_COMPRA_CODIGO,
 			DESCUENTO_COMPRA_VALOR
 	FROM [gd_esquema].[Maestra]
-	WHERE DESCUENTO_COMPRA_CODIGO is not null
+	WHERE DESCUENTO_COMPRA_CODIGO IS NOT NULL
+
 --COMMIT
 ROLLBACK
+
+----------------------------------------------
+SELECT * FROM [sale_worson].[descuento_compra]
+----------------------------------------------
+
+
+-------------------------------------------------------- COMPRA X DESCUENTO --------------------
+BEGIN TRANSACTION
+	INSERT INTO [sale_worson].[compra_x_descuento]
+
+	SELECT DISTINCT
+		c.COMPRA_NUMERO,
+		dc.DESCUENTO_COMPRA_CODIGO
+
+	FROM  [gd_esquema].[Maestra] m
+
+	INNER JOIN [sale_worson].[compra] c ON m.COMPRA_NUMERO = c.COMPRA_NUMERO
+	INNER JOIN [sale_worson].[descuento_compra] dc ON m.DESCUENTO_COMPRA_CODIGO = dc.DESCUENTO_COMPRA_CODIGO
+
+--COMMIT
+ROLLBACK
+
+------------------------------------------------
+SELECT * FROM [sale_worson].[compra_x_descuento]
+------------------------------------------------
+
